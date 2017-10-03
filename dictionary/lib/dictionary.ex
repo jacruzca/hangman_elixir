933cee2f78398a8b4a9034311373659bc1fc7b91
jhon.cruz <jhon@beepquest.com>
2017-10-02 21:56:01 -0500

    Added my first supervisor

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,6 +1,5 @@
 defmodule Dictionary do
 
-  defdelegate start(), to: Dictionary.WordList
-  defdelegate random_word(word_list), to: Dictionary.WordList
+  defdelegate random_word(), to: Dictionary.WordList
 
 end
